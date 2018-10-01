22a76756129145e43a7241f3e96adbc2820fccb7
BSorbus <BSorbus@gmail.com>
2018-10-01 20:12:37 +0200

    Better certificate layout

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,8 +16,8 @@ class User
       # uri = URI("http://localhost:3000/api/v1/login")
       uri = URI("#{Rails.application.secrets[:netpar2015_api_url]}/login")
       http = Net::HTTP.new(uri.host, uri.port)
-      http.use_ssl = true
-      http.verify_mode = OpenSSL::SSL::VERIFY_NONE # Sets the HTTPS verify mode
+      #http.use_ssl = true
+      #http.verify_mode = OpenSSL::SSL::VERIFY_NONE # Sets the HTTPS verify mode
       req = Net::HTTP::Post.new(uri.path, {'Content-Type' => 'application/json'})
       req.body = {"email" => "#{self.email}", "password" => "#{self.password}"}.to_json
       res = http.request(req)
